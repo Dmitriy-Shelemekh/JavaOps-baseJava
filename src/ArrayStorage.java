839e47a47dbcfe0a526a854625cbaca628489eab
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-09-01 22:33:13 +0300

    На проверку

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,56 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private int arrMaxSize = 10000;
+    private Resume[] storage = new Resume[arrMaxSize];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        if (get(r.uuid) == null && size() < arrMaxSize) {
+            storage[size()] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int count = 0;
+
+        for (Resume r : storage) {
+            if (r != null) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
