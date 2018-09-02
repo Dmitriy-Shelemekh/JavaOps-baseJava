f4ddfa984d4b5e4db5d1a66126c0babf9f822291
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-09-02 18:34:25 +0300

    Изменил метод delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,3 @@
-import org.apache.commons.lang.ArrayUtils;
-
 import java.util.Arrays;
 
 /**
@@ -16,14 +14,14 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        if (get(r.uuid) == null && size() < RESUME_LIMIT) {
-            storage[size()] = r;
+        if (get(r.uuid) == null && storageSize < RESUME_LIMIT) {
+            storage[storageSize] = r;
             storageSize++;
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < storageSize; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 return storage[i];
             }
@@ -33,10 +31,23 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < storageSize; i++) {
             if (uuid.equals(storage[i].uuid)) {
-                Resume[] tmp = storage;
-                storage = (Resume[]) ArrayUtils.addAll(ArrayUtils.remove(tmp, i), new Resume[1]);
+
+                Resume[] arrStart = Arrays.copyOfRange(storage, 0, i);
+                Resume[] arrEnd = Arrays.copyOfRange(storage, i + 1, storage.length);
+
+                Resume[] arrResult = new Resume[arrStart.length + arrEnd.length + 1];
+
+                System.arraycopy(arrStart, 0, arrResult, 0, arrStart.length);
+                System.arraycopy(arrEnd, 0, arrResult, arrStart.length, arrEnd.length);
+
+                storage = arrResult;
+
+//                  Это решения лучше.
+//                Resume[] tmp = storage;
+//                storage = (Resume[]) ArrayUtils.addAll(ArrayUtils.remove(tmp, i), new Resume[1]);
+
                 storageSize--;
                 return;
             }
@@ -47,7 +58,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, storageSize);
     }
 
     int size() {
