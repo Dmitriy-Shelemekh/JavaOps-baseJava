060984e37bb99ef7e5c2b276813625ef2a879a81
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-09-09 20:30:57 +0300

    Рефакторинг текста ошибок.

--- src/ErrorMessages.java
+++ src/ErrorMessages.java
@@ -1,7 +1,7 @@
 public enum ErrorMessages {
-    NO_IN_STORAGE_ERROR("Такого резюме нет в хранилище."),
-    NO_FREE_SPACE_ERROR("В хранилище резюме больше нет свободного места."),
-    ALREADY_EXIST_NAME_ERROR("Резюме с таким названием уже есть в хранилище.");
+    NO_IN_STORAGE_ERROR("Резюме нет в хранилище."),
+    NO_FREE_SPACE_ERROR("В хранилище нет свободного места."),
+    ALREADY_EXIST_NAME_ERROR("Такое резюме уже есть в хранилище.");
 
     String errorMsg;
     int code;
