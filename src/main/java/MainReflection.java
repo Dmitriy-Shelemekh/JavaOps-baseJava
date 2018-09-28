10723ffdbc2b031d4a4f9e1a5f30c1830d1d740c
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-09-28 18:08:22 +0300

    Рифлексия.

---
+++ src/main/java/MainReflection.java
@@ -0,0 +1,20 @@
+import model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume();
+        Class resumeClass = resume.getClass();
+
+        Field fieldUuid = resumeClass.getDeclaredField("uuid");
+        fieldUuid.setAccessible(true);
+        fieldUuid.set(resume, "New_name");
+
+        Method methodToString = resumeClass.getDeclaredMethod("toString");
+
+        System.out.println(methodToString.invoke(resume));
+    }
+}
