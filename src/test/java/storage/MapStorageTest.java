4ffef79366ec3523b995b875cbe7c7b14057c7a3
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-12-13 08:08:23 +0300

    HW5. Готово. На проверку

--- src/test/java/storage/MapStorageTest.java
+++ src/test/java/storage/MapStorageTest.java
@@ -1,7 +1,14 @@
 package storage;
 
+import org.junit.Test;
+
 public class MapStorageTest extends AbstractStorageTest {
     public MapStorageTest() {
         super(new MapStorage());
+    }
+
+    @Override
+    @Test()
+    public void testStorageOverflow() {
     }
 }
