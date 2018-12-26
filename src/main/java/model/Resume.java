f41e6996427a79211334056936014447f7c0d180
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-12-26 23:30:07 +0300

    HW6. Add fullName field to resume model

--- src/main/java/model/Resume.java
+++ src/main/java/model/Resume.java
@@ -3,12 +3,27 @@ package model;
 public class Resume implements Comparable<Resume> {
     private String uuid;
 
+    private String fullName;
+
+    public Resume(String uuid, String fullName) {
+        this.uuid = uuid;
+        this.fullName = fullName;
+    }
+
     public Resume(String uuid) {
         this.uuid = uuid;
     }
 
     public Resume() {
 
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getUuid() {
