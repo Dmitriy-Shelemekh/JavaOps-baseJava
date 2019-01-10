613b35ab69ba71a37786ff8d8c85b069da0011ec
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2019-01-11 00:12:15 +0300

    HW6. Add comparator to SortedArrayStorage.

--- src/main/java/storage/SortedArrayStorage.java
+++ src/main/java/storage/SortedArrayStorage.java
@@ -3,14 +3,15 @@ package storage;
 import model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-//    private static final Comparator<Resume> RESUME_COMPARATOR = ((o1, o2) -> o1.getUuid().compareTo(o2.getUuid()));
+    private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
 
     @Override
     public Integer getElementSearchKey(String uuid) {
-        Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        Resume searchKey = new Resume(uuid, "Some Name");
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 
     @Override
