f08510694a678ef9b23084948f73ecfb5e31de4a
Dmitriy_Shelemekh <d.shelemekh@gmail.com>
2019-07-13 14:33:50 +0300

    HW9 Красивый вывод в MainFile

--- src/main/java/MainFile.java
+++ src/main/java/MainFile.java
@@ -29,19 +29,19 @@ public class MainFile {
             throw new RuntimeException(e);
         }
 
-        printDirectoryDeeply(dir);
+        printDirectoryDeeply(dir, "");
     }
 
-    private static void printDirectoryDeeply(File dir) {
+    public static void printDirectoryDeeply(File dir, String offet) {
         File[] files = dir.listFiles();
 
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(offet + "F: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectoryDeeply(file);
+                    System.out.println(offet + "D: " + file.getName());
+                    printDirectoryDeeply(file, offet + "  ");
                 }
             }
         }
